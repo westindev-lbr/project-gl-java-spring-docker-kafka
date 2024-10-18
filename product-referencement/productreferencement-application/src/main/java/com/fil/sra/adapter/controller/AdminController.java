e80d28b726c0b015c74a397529170f0c4acf4fdc
Jean emmanuel Ehuy <jeanemmanuelkarld.ehuy.etu@univ-lille.fr>
2024-10-18 13:10:01 +0200

    Adding Query Params

--- product-referencement/productreferencement-application/src/main/java/com/fil/sra/adapter/controller/AdminController.java
+++ product-referencement/productreferencement-application/src/main/java/com/fil/sra/adapter/controller/AdminController.java
@@ -18,8 +18,22 @@ public class AdminController {
     public AdminController(IArticleUseCases articleUseCases){
         this.articleUseCases = articleUseCases;
     }
-    @GetMapping("/articles")
-    public ResponseEntity<List<ArticleDto>> getPaginatedArticles(@RequestBody ResearchArticleRequestDto search){
+    @GetMapping("/articles/search")
+    public ResponseEntity<List<ArticleDto>> getPaginatedArticles(
+            @RequestParam(required = false) List<String> categories,
+            @RequestParam(required = false) String subName,
+            @RequestParam(defaultValue = "10") int paginationSize,
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(required = false) Integer articleId){
+
+        ResearchArticleRequestDto search = ResearchArticleRequestDto.builder()
+                .articleId(articleId)
+                .subName(subName)
+                .categories(categories)
+                .paginationSize(paginationSize)
+                .pageNumber(pageNumber)
+                .build();
+
         return ResponseEntity.ok(articleUseCases.getPaginatedArticles(search));
     }
 
