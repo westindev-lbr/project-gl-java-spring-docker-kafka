2782d422b2d5134e0dde0d8c89fcebae8e8c3957
Axel Labarre <axel.labarre.etu@univ-lille.fr>
2024-10-20 06:34:48 +0200

    ✨ : update config and AdminController

--- product-referencement/productreferencement-application/src/main/java/com/fil/sra/adapter/config/UseCaseConfig.java
+++ product-referencement/productreferencement-application/src/main/java/com/fil/sra/adapter/config/UseCaseConfig.java
@@ -1,15 +1,25 @@
 package com.fil.sra.adapter.config;
 
-import com.fil.sra.interfaces.IArticleRepository;
+import com.fil.sra.ports.IArticleRepository;
 import com.fil.sra.ports.IArticleUseCases;
+import com.fil.sra.ports.IStockRepository;
+import com.fil.sra.ports.IStockUseCase;
 import com.fil.sra.services.ArticleUseCasesImpl;
+import com.fil.sra.services.StockUseCaseImpl;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UseCaseConfig {
     @Bean
-    public IArticleUseCases articleUseCases(IArticleRepository articleRepository){
+    public IArticleUseCases articleUseCases(IArticleRepository articleRepository) {
         return new ArticleUseCasesImpl(articleRepository);
+    }
+
+    @Bean
+    public IStockUseCase stockUseCase(@Qualifier("stockRepository") IStockRepository stockRepository) {
+        return new StockUseCaseImpl(stockRepository);
     }
 }
