340ed52f1a1cd131fefe4faf40e5c974eba108f1
thomas.desruelle.etu <thomas.desruelle.etu@univ-lille.fr>
2024-10-21 22:08:36 +0200

    :tada: : init branch

---
+++ discount-management/discountmanagement-application/src/main/java/com/fil/sra/controller/MarketOperationController.java
@@ -0,0 +1,10 @@
+package com.fil.sra.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller(value = "/market-operation")
+public class MarketOperationController {
+
+    public ResponseEn
+
+}
