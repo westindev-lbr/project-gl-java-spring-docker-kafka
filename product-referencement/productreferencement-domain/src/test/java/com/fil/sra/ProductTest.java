0701e440b58e21da500ad9a6138c72b42c9485d3
Axel Labarre <axel.labarre.etu@univ-lille.fr>
2024-10-22 16:06:18 +0200

    🔥 : fix with removing ProductTest

--- product-referencement/productreferencement-domain/src/test/java/com/fil/sra/ProductTest.java
+++
@@ -1,9 +0,0 @@
-package com.fil.sra;
-
-import com.fil.sra.models.Product;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class ProductTest extends ArticleTest{
-}
